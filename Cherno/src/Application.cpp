4ef2bfd349da7e916b3657102a74c0d051ca1811
MakGris <maxgrishenko@gmail.com>
2024-02-02 18:02:50 +0700

    add uniforms

--- Cherno/src/Application.cpp
+++ Cherno/src/Application.cpp
@@ -130,6 +130,8 @@ int main()
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    //ограничение фреймрейта
+    glfwSwapInterval(2);
 
    
     if (glewInit() != GLEW_OK) 
@@ -178,18 +180,24 @@ int main()
     GLCall(int location = glGetUniformLocation(shader, "u_Color"));
     ASSERT(location != -1);
 
-    GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
+    GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
 
-
+    float r = 0.0f;
+    float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-       
+        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
-        
+
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+            increment = 0.05f;
+        r += increment;
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
