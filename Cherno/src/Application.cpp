43103462d3f32e3204fa523a4ff0ae5627b08b15
MakGris <maxgrishenko@gmail.com>
2024-02-06 18:20:28 +0700

    Making classes

--- Cherno/src/Application.cpp
+++ Cherno/src/Application.cpp
@@ -4,27 +4,12 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include "Renderer.h"
 
-#define ASSERT(x) if (!(x)) __debugbreak();
-#define GLCall(x) GLClearError();\
-    x;\
-    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
 
 
-static void GLClearError()
-{
-    while (glGetError() != GL_NO_ERROR);
-}
-
-static bool GLLogCall(const char* function, const char* file, int line) 
-{
-    while (GLenum error = glGetError())
-    {
-        std::cout << "[OpenGL Error] (" << error << ")" << function <<" " << file << ":" << line << std::endl;
-        return false;
-    }
-    return true;
-}
 
 struct ShaderProgramSource
 {
@@ -143,92 +128,86 @@ int main()
     }
     //Отображение текущей версии OpenGL
     std::cout << glGetString(GL_VERSION) << std::endl;
-
-    //координаты позиций вершин (Vertex)
-    float positions[] = {
-        -0.5f, -0.5,    // 0
-         0.5f, -0.5f,   // 1
-         0.5f, 0.5f,    // 2
-        -0.5f, 0.5f     // 3
-    };
-
-    unsigned int indices[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
-    //серия 4, создаем буфер, который хранит информацию для отрисовки на GPU
-    
-    //серия 12, создаем вручную vertex array c core profile (при использовании core vertex array не создается автоматически)
-    unsigned int vao;
-    GLCall(glGenVertexArrays(1, &vao));
-    GLCall(glBindVertexArray(vao));
-
-
-    unsigned int buffer;
-    GLCall(glGenBuffers(1, &buffer));
-    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
-    GLCall(glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW));
-    
-    //Серия 5, создаем атрибуты и активируем их, для того, чтобы видеокарта понимала, как рисовать вершины и что они содержат
-    GLCall(glEnableVertexAttribArray(0));
-    GLCall(glVertexAttribPointer(0  , 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
-    // создание  index buffer, нужен для того, чтобы не хранить в памяти повторяющиеся вершины треугольника для отрисовки квадрата
-    unsigned int ibo;
-    GLCall(glGenBuffers(1, &ibo));
-    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
-    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
-
-    ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
-    std::cout << "VERTEX" << std::endl;
-    std::cout << source.VertexSource << std::endl;
-    std::cout << "FRAGMENT" << std::endl;
-    std::cout << source.FragmentSource << std::endl;
-    
-    unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-    GLCall(glUseProgram(shader));
-
-    GLCall(int location = glGetUniformLocation(shader, "u_Color"));
-    ASSERT(location != -1);
-
-    GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
-    
-    //unbind buffer
-    GLCall(glBindVertexArray(0));
-    GLCall(glUseProgram(0));
-    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
-    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
-   
-
-    float r = 0.0f;
-    float increment = 0.05f;
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        //координаты позиций вершин (Vertex)
+        float positions[] = {
+            -0.5f, -0.5,    // 0
+             0.5f, -0.5f,   // 1
+             0.5f, 0.5f,    // 2
+            -0.5f, 0.5f     // 3
+        };
 
-        GLCall(glUseProgram(shader));
-        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+        unsigned int indices[] = {
+            0, 1, 2,
+            2, 3, 0
+        };
 
+
+        //серия 12, создаем вручную vertex array c core profile (при использовании core vertex array не создается автоматически)
+        unsigned int vao;
+        GLCall(glGenVertexArrays(1, &vao));
         GLCall(glBindVertexArray(vao));
-        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
-        
-        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
-        if (r > 1.0f)
-            increment = -0.05f;
-        else if (r < 0.0f)
-            increment = 0.05f;
-        r += increment;
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        VertexBuffer vb(positions, 4 * 2 * sizeof(float));
 
-        /* Poll for and process events */
-        glfwPollEvents();
+
+        //Серия 5, создаем атрибуты и активируем их, для того, чтобы видеокарта понимала, как рисовать вершины и что они содержат
+        GLCall(glEnableVertexAttribArray(0));
+        GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
+        // создание  index buffer, нужен для того, чтобы не хранить в памяти повторяющиеся вершины треугольника для отрисовки квадрата
+        IndexBuffer ib(indices, 6);
+
+        ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
+        std::cout << "VERTEX" << std::endl;
+        std::cout << source.VertexSource << std::endl;
+        std::cout << "FRAGMENT" << std::endl;
+        std::cout << source.FragmentSource << std::endl;
+
+        unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+        GLCall(glUseProgram(shader));
+
+        GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+        ASSERT(location != -1);
+
+        GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
+
+        //unbind buffer
+        GLCall(glBindVertexArray(0));
+        GLCall(glUseProgram(0));
+        GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
+
+        float r = 0.0f;
+        float increment = 0.05f;
+        /* Loop until the user closes the window */
+        while (!glfwWindowShouldClose(window))
+        {
+            /* Render here */
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            GLCall(glUseProgram(shader));
+            GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+
+            GLCall(glBindVertexArray(vao));
+            ib.Bind();
+
+            GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+            if (r > 1.0f)
+                increment = -0.05f;
+            else if (r < 0.0f)
+                increment = 0.05f;
+            r += increment;
+            /* Swap front and back buffers */
+            glfwSwapBuffers(window);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
+
+        glDeleteProgram(shader);
     }
-
-    glDeleteProgram(shader);
-
     glfwTerminate();
     return 0;
 }
