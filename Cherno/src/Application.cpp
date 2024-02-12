a34307a0632fb4d90d0be778f59a3a5a4e25153b
MakGris <maxgrishenko@gmail.com>
2024-02-12 19:38:13 +0700

    abstract Vertex Array

--- Cherno/src/Application.cpp
+++ Cherno/src/Application.cpp
@@ -4,10 +4,12 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+
 #include "Renderer.h"
 
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
+#include "VertexArray.h"
 
 
 
@@ -148,12 +150,14 @@ int main()
         GLCall(glGenVertexArrays(1, &vao));
         GLCall(glBindVertexArray(vao));
 
+        VertexArray va;
         VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+        VertexBufferLayout layout;
+        layout.Push<float>(2);
+        va.AddBuffer(vb, layout);
 
 
-        //Серия 5, создаем атрибуты и активируем их, для того, чтобы видеокарта понимала, как рисовать вершины и что они содержат
-        GLCall(glEnableVertexAttribArray(0));
-        GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
+       
         // создание  index buffer, нужен для того, чтобы не хранить в памяти повторяющиеся вершины треугольника для отрисовки квадрата
         IndexBuffer ib(indices, 6);
 
@@ -189,7 +193,7 @@ int main()
             GLCall(glUseProgram(shader));
             GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 
-            GLCall(glBindVertexArray(vao));
+            va.Bind();
             ib.Bind();
 
             GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
